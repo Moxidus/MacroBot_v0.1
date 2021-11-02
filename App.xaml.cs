bffec63d62653d418ab71b03fbea601a6efd7978
Dominik Kutynsky <dominikkutynsky@gmail.com>
2021-11-03 00:57:06 +0100

    Added opening and closing of .mcr files

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,21 @@ namespace MacroBot_v0._1
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow(e.Args);
+            mainWindow.Show();
+        }
+
+        private static void Associate()
+        {
+            RegistryKey FileReg = Registry.CurrentUser.CreateSubKey("Software\\Classes\\.mcr");
+            RegistryKey AppReg = Registry.CurrentUser.CreateSubKey("Software\\Classes\\Application\\MacroBot_v0.1.exe");
+            RegistryKey AppAssoc = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\CurrentVersion\\Explorer\\FileExts\\.mcr");
+
+
+        }
+
+
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,52 @@ namespace MacroBot_v0._1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(string[] Args)
         {
             InitializeComponent();
+
+            //MessageBox.Show("DEBUG LMAO:" + thing);
+
+            if(Args.Length != 0)
+                OpenFile(Args[0].ToString());
+        }
+
+        public void OpenFile(string path)
+        {
+            try
+            {
+                string scriptText = File.ReadAllText(path);
+
+                scriptCode.Document.Blocks.Clear();
+                scriptCode.Document.Blocks.Add(new Paragraph(new Run(scriptText)));
+            }
+            catch (Exception ex)// This is here incase there is error with the file name
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        public void SaveFile(object sender, RoutedEventArgs e)
+        {
+            string scriptText = new TextRange(scriptCode.Document.ContentStart, scriptCode.Document.ContentEnd).Text;// Gets the whole code in string
+
+
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "MacroScript"; // Default file name
+            dlg.DefaultExt = ".mcr"; // Default file extension
+            dlg.Filter = "Macro script file (.mcr)|*.mcr"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                File.WriteAllText(filename, scriptText);
+            }
+
         }
     }
 }
