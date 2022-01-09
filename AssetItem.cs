35b6bb9e2ff3177b14fe5d7a757c3eca45c08dfa
Dominik Kutynsky <dominikkutynsky@gmail.com>
2022-01-09 22:18:39 +0100

    Takes screenshots and saves them into assets

---
+++ AssetItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace MacroBot_v0._1
+{
+    class AssetItem
+    {
+        public AssetItem(Image<Bgr, byte> asset, string name)
+        {
+            this.asset = asset;
+            this.name = name;
+        }
+
+        public readonly Image<Bgr, byte> asset;
+        public readonly string name;
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
